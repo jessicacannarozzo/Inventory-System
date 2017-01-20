9061a1fc7ccbd5620943cd8431dcf4d0a5f2c3c6
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-01-20 14:39:39 -0500

    Added skeleton code by Dr. Christine Laurendeau

---
+++ Customer.h
@@ -0,0 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                 */
+/*  Program:  Simple Inventory System              */
+/*  Author:   Christine Laurendeau                 */
+/*  Date:     28-JUN-2016                          */
+/*                                                 */
+/*  (c) 2016 Christine Laurendeau                  */
+/*  All rights reserved.  Distribution and         */
+/*  reposting, in part or in whole, without the    */
+/*  written consent of the author, is illegal.     */
+/*                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+#include "Product.h"
+
+class Customer
+{
+  public:
+    Customer(string="Unknown");
+    int    getId();
+    string getName();
+    int    getPoints();
+  protected:
+    static int nextCustId;
+    int        id;
+    string     name;
+    int        points;
+};
+
+#endif
