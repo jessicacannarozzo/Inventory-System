a0f68d0c85910ea64381650b9a4a719c665d438a
Karla Martins Spuldaro <karlamartinsspuldaro@cmail.carleton.ca>
2017-01-29 17:12:14 -0500

    included getPurchase in PurchArray

--- PurchArray.cc
+++ PurchArray.cc
@@ -10,7 +10,13 @@ PurchArray::PurchArray() {
 
 int PurchArray::getPurchArraySize() { return purchSize; } //get the size
 
-Purchase* PurchArray::getPurchArray() { return *pArr; }
+// Purchase* PurchArray::getPurchArray() { return *pArr; } //K: this shouldn't be exposed to any other class
+
+
+Purchase& PurchArray::getPurchase(int index){
+	return &(pArr[index]);
+}
+
 
 
 
@@ -26,7 +32,7 @@ void PurchArray::addNewPurchase(Product& p)
   	}
 
   	//we didn't find the item if we get here. Customer has never bought this item before.
-	Purchase* newPurch = new Purchase(p); // memory leak!
+	Purchase* newPurch = new Purchase(p); // K: memory leak!
 	pArr[purchSize] = newPurch; // assuming purchSize is always < MAX_ARR for this assignment
 	purchSize++; 
 	return;
