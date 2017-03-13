7f9d82427bdf995da2fbc79354ce99a848df3b27
Karla Martins Spuldaro <karlamartinsspuldaro@cmail.carleton.ca>
2017-03-13 11:34:46 -0400

    adding OrderServer files  -- Oops

---
+++ OrderServer.cc
@@ -0,0 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                 */
+/* Authors: Jess Cannarozzo (101007447)            */
+/*          Karla Martins Spuldaro (101021516)     */
+/*                                                 */
+/* Class Def: OrderArray.cc                        */
+/* Array of product purchases by customers.        */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "OrderServer.h"
+
+//OrderServer::OrderServer() {}
+
+//OrderServer::~OrderServer() {}
+
+void OrderServer::update(Order* order)
+{
+    orders.addOrder(order);
+}
+
+
+void OrderServer::retrieve(OrderArray& arr)
+{
+  // Fill arr with orders
+  for(int i = 0; i < orders.getOrderSize(); i++)
+  {
+    arr.addOrder(orders.getOrder(i));
+  }
+}
