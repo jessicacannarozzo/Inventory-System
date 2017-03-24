67b0578976a5235660841b79928215603eecf5b8
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-03-24 13:41:14 -0400

    init Date.cc and Date.h

---
+++ Date.cc
@@ -0,0 +1,8 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                 */
+/* Authors: Jess Cannarozzo (101007447)            */
+/*          Karla Martins Spuldaro (101021516)     */
+/*                                                 */
+/* Class Def: Date.cc                              */
+/* MONTH/DAY/YEAR                                  */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
---
+++ Date.h
@@ -0,0 +1,8 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                 */
+/* Authors: Jess Cannarozzo (101007447)            */
+/*          Karla Martins Spuldaro (101021516)     */
+/*                                                 */
+/* Class Def: Date.h                              */
+/* MONTH/DAY/YEAR                                  */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
