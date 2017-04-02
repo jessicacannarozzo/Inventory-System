3a8b9c6fb26d051dbde73baff84bf42f9431c9bf
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-04-01 21:35:37 -0400

    header added

---
+++ CustList.cc
@@ -0,0 +1,9 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                 */
+/* Authors: Jess Cannarozzo (101007447)            */
+/*          Karla Martins Spuldaro (101021516)     */
+/*                                                 */
+/* Class Def: CustList.cc                          */
+/* List of customers that may buy products from    */
+/* the store.                                      */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
